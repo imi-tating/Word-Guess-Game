c47523ccb0a332af58ef039a0365d180af77e0c0
Mandi Blanton <imitating@yahoo.com>
2018-07-03 18:16:47 -0700

    keyup letter input working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,10 @@
-alert("hi");
+var wordsToBeGuessed = ["cat", "meow", "mew", "kitten", "furball", "purrball", "hairball", "purrito"];
+
+var wordToBeGuessed = [Math.floor(Math.random() * wordsToBeGuessed.length)];
+
+document.onkeyup = function(event) {
+  var letterGuess = event.key;
+
+  console.log(letterGuess);
+
+}
