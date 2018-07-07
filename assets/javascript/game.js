1adae05dd9336b0387cfe5d0037b23729b496a74
Mandi Blanton <imitating@yahoo.com>
2018-07-06 17:43:05 -0700

    word blanks are now generating onto page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,34 @@
 var wordsToBeGuessed = ["cat", "meow", "mew", "kitten", "furball", "purrball", "hairball", "purrito"];
 
-var wordToBeGuessed = [Math.floor(Math.random() * wordsToBeGuessed.length)];
+var wordToBeGuessed = wordsToBeGuessed[Math.floor(Math.random() * wordsToBeGuessed.length)];
+console.log(wordToBeGuessed);
 
 document.onkeyup = function(event) {
   var letterGuess = event.key;
+  var guessThisWord = $("#wordToBeGuessed");
 
-  console.log(letterGuess);
+
+
+  function createWordBlank () {
+    var starterLetterBlank = "_";
+    var starterWordBlank = [];
+    for(var i = 0; i < (wordToBeGuessed.length); i++){
+      starterWordBlank.push(starterLetterBlank);
+    }
+    starterWordBlank = starterWordBlank.join(" ");
+    guessThisWord.text(starterWordBlank);
+  }
+
+
+
+    createWordBlank();
+
+
+
+
+  //console.log(starterWordBlank);
+
+  //document.querySelector(#wordToBeGuessed).textContent = ;
+
 
 }
